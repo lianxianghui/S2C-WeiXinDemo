abd49668ba671f4df5201b745bcada31b20cc2ce
Lian <lianxianghui@hotmail.com>
2021-08-09 15:38:05 +0800

    初始提交

---
+++ WeiXinDemo-iOS/WeiXinDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiXinDemo
+//
+//  Created by lian on 2021/8/5.
+//  Copyright © 2021年 lianxianghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
